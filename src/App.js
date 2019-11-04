759f93b06d57072c76386e62afda2a1f0b183dfc
Minhtality <37172196+Minhtality@users.noreply.github.com>
2019-11-03 19:32:49 -0800

    initialized redux

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import SearchBar from './components/layout/SearchBar';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
 import Logs from './components/logs/Logs'
-import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
-import './App.css';
 import AddTechModal from './components/techs/AddTechModal';
 import EditLogModal from './components/logs/EditLogModal';
 import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
+import 'materialize-css/dist/css/materialize.min.css';
+import './App.css';
 
 const App = () => {
 
@@ -18,17 +20,20 @@ const App = () => {
   });
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+
   );
 }
 
